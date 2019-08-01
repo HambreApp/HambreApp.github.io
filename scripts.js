56ae9d8da1db981869f09201d66eba6ccc78b396
Spencer <53015365+SpencerLeff@users.noreply.github.com>
2019-07-31 20:08:59 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 //var Restaurants = ["Grana", "Momona", "Gogis", "MadisonBear", "Tres Hombres", "Ali Baba", "Peeking Chinese", "Celestino's Pizza", "Cheese Steakshop", "Great Northern Coffee"]
-function Restaurants (name, link) {}
+function Restaurants(name, link) {}
 
-var imgArray[];
-imgArray[0] = new Restaurants("Grana", "Grana.jpg")
+var imgArray = new Array();
+imgArray.push(new Restaurants("Grana", "Grana.jpg"));
 
-imgArray[1] = new Restaurants("Momona", "Momona.jpg")
+imgArray.push(new Restaurants("Momona", "Momona.jpg"));
 
-imgArray[2] = new Restaurants("MadisonBear", "MadisonBearGarden.jpg")
+imgArray.push(new Restaurants("MadisonBear", "MadisonBearGarden.jpg"));
 
-imgArray[3] = new Restaurants("Gogis", "GogisCafe.jpg")
+imgArray.push(new Restaurants("Gogis", "GogisCafe.jpg"));
 
-imgArray[4] = new Restaurants("CheeseSteakShop", "CheeseSteakShop.jpg")
+imgArray.push(new Restaurants("CheeseSteakShop", "CheeseSteakShop.jpg"));
 
-imgArray[5] = new Restaurants("CelestinosPizza", "CelestinosPizza.jpg")
+imgArray.push(new Restaurants("CelestinosPizza", "CelestinosPizza.jpg"));
 
-imgArray[6] = new Restaurants("AliBaba", "AliBaba.jpg")
+imgArray.push(new Restaurants("AliBaba", "AliBaba.jpg"));
 
-imgArray[7] = new Restaurants("PeekingChinese", "PeekingChinese.jpg")
+imgArray.push(new Restaurants("PeekingChinese", "PeekingChinese.jpg"));
 
-imgArray[8] = new Restaurants("TresHombres", "TresHombres.jpg")
+imgArray.push(new Restaurants("TresHombres", "TresHombres.jpg"));
 
-imgArray[9] = new Restaurants("GreatNorthernCoffee", "GreatNorthernCoffee.jpg")
+imgArray.push(new Restaurants("GreatNorthernCoffee", "GreatNorthernCoffee.jpg"));
 
 /*------------------------------------*/
 
